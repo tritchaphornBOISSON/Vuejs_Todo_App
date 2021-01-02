deea7fad42a81c25a872ce35555ed422f91e00d2
Tritchaphorn Boisson <tritchaphorn@gmail.com>
2021-01-02 18:22:57 +0100

    fitst commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <div class="container">
+<Header />
+    <router-view/>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header';
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #42b983;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+.container {
+    max-width: 1100px;
+    margin: auto;
+    overflow: auto;
+    padding: 0 2rem;
+}
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.6;
+    background: #e8f7f0;
+
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="notes">
+<div class="todo-item " v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+            <button @click="$emit('del-todo', todo.id)" class="del">X</button>
+            </p>
+    </div>
+</div>
+    
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"],
+    methods: {
+        markComplete() {
+            this.todo.completed = !this.todo.completed;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+    .todo-item {
+         border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+    }
+    .is-complete {
+        text-decoration: line-through;
+    }
+    .del {
+        background: #ff0000;
+        color: #fff;
+        border: none;
+        padding: 5px 9px;
+        border-radius: 50%;
+        cursor: pointer;
+        float: right;
+    }
+</style>
